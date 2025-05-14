d006bc8a60a151fdf2143ed72294862cc6cb8ad3
yonnik <137620324+yonnik@users.noreply.github.com>
2025-05-14 22:24:12 +0100

    updated express version

--- backend/index.js
+++ backend/index.js
@@ -32,9 +32,9 @@ app.use("/api", questionRoute)
 if(process.env.NODE_ENV ==="production"){
   app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-  app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-  })
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 } //servers static files. path to the static assets that has the html and css files. goes 1 up into the main folder then into frontend. 
 
 
