37c5e76cb3de5f333923bb1ebe5af856be9d64df
yonnik <137620324+yonnik@users.noreply.github.com>
2025-05-14 20:22:19 +0100

    project completed

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ dotenv.config();
 const app = express()
 
 const PORT = process.env.PORT ;
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json());
@@ -27,6 +28,14 @@ app.use(cors({
 
 
 app.use("/api", questionRoute)
+
+if(process.env.NODE_ENV ==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+  app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+} //servers static files. path to the static assets that has the html and css files. goes 1 up into the main folder then into frontend. 
 
 
 
